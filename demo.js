7261f831d3609fb6587f0e3dbd6a820904f4afac
pramodkasulanati <saipramodk@gmail.com>
2018-05-08 01:45:17 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,7 +1,36 @@
-//Insert code below
 
-function sayHello()
-{
-               alert("May the odds be in your favour!");
-               console.log("Hello World!");
+
+<script>
+var myInput = document.getElementById("psw");
+
+
+myInput.onkeyup = function() {
+
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+  
+  } else {
+    
+  }
+
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    
+  } else {
+    
+  }
+
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    
+  } else {
+  
+  }
+
+  if(myInput.value.length >= 8) {
+  
+  } else {
+    
+  }
 }
+</script>
